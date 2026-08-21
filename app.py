057d37e9348708beb493488d615ab2858425e2cd
Tanmay Mehta <10maymehta@gmail.com>
2026-08-21 15:24:00 +0530

    check_text_extraction

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 from dotenv import load_dotenv
-from Py2PDF import PdfReader
+from PyPDF2 import PdfReader
 
 def get_pdf_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
         pdf_reader = PdfReader(pdf)
-        for page in pdf_reader.page():
+        for page in pdf_reader.pages:
             text += page.extract_text()
     return text
 
@@ -21,7 +21,8 @@ def main():
         pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
         if st.button("Process"):
             with st.spinner("Processing..."):
-
+                raw_text = get_pdf_text(pdf_docs)
+                st.write(raw_text)
 
 if __name__ == '__main__':
     main()

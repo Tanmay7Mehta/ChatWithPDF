22c415830cc88ca6ef8ec3c3f0bea4b335f5b1ef
Tanmay Mehta <10maymehta@gmail.com>
2026-08-21 15:33:09 +0530

    chunking

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -9,6 +10,16 @@ def get_pdf_text(pdf_docs):
         for page in pdf_reader.pages:
             text += page.extract_text()
     return text
+
+def get_text_chunks(text):
+    text_spiletter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_spiletter.split_text(text)
+    return chunks
 
 def main():
     st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
@@ -22,7 +33,10 @@ def main():
         if st.button("Process"):
             with st.spinner("Processing..."):
                 raw_text = get_pdf_text(pdf_docs)
-                st.write(raw_text)
+                #st.write(raw_text)
+
+                text_chunks = get_text_chunks(raw_text)
+                st.write(text_chunks)
 
 if __name__ == '__main__':
     main()

f455e58c0e17f38bb4561ef8bb331c2f36c12a47
Tanmay Mehta <10maymehta@gmail.com>
2026-08-21 15:16:03 +0530

    text_extraction

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 import streamlit as st
+from dotenv import load_dotenv
+from Py2PDF import PdfReader
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.page():
+            text += page.extract_text()
+    return text
 
 def main():
     st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
@@ -8,8 +18,10 @@ def main():
 
     with st.sidebar:
         st.subheader("Your Documents")
-        st.file_uploader("Upload your PDFs here and click on 'Process'")
-        st.button("Process")
+        pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing..."):
+
 
 if __name__ == '__main__':
     main()

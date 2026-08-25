d5481b67712f991c24dc24252c9711914df28149
Tanmay Mehta <10maymehta@gmail.com>
2026-08-25 16:25:15 +0530

    chunking

--- project.py
+++ project.py
@@ -15,3 +15,13 @@ def get_pdf_text(pdf_docs):
         for page in pdf_reader.pages:
             text += page.extract_text()
     return text
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks

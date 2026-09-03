7f291a142ae6999cf7a43ac3178032b5cdfafe6e
Tanmay Mehta <10maymehta@gmail.com>
2026-09-03 20:10:35 +0530

    text_extraction

--- test.py
+++ test.py
@@ -8,3 +8,11 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain_groq import ChatGroq
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.page:
+            text += page.extract_text()
+    return text

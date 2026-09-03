a134d334114d048a04784aba36ebe90119851ad5
Tanmay Mehta <10maymehta@gmail.com>
2026-09-03 20:08:19 +0530

    importing

---
+++ test.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template
+from langchain_groq import ChatGroq

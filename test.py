3d04cc3caa93c6faded3b23b69068691822c2c67
Tanmay Mehta <10maymehta@gmail.com>
2026-09-03 20:13:08 +0530

    text_chunks

--- test.py
+++ test.py
@@ -16,3 +16,13 @@ def get_pdf_text(pdf_docs):
         for page in pdf_reader.page:
             text += page.extract_text()
     return text
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
